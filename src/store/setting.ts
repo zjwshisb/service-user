bf7444755ec4d7c57e172a3177a98a1e74358dbd
周建威 <361340272@qq.com>
2025-01-01 17:31:11 +0800

    u

--- src/store/setting.ts
+++ src/store/setting.ts
@@ -1,15 +1,6 @@
 import { observable } from 'mobx'
-import {getSetting} from "@/api";
 
 const settingStore = observable({
-  is_show_read: false,
-  is_show_queue: false,
-  refresh() {
-    getSetting().then(res => {
-      this.is_show_queue = res.data.is_show_queue
-      this.is_show_read = res.data.is_show_read
-    })
-  }
 })
 
 export default settingStore
