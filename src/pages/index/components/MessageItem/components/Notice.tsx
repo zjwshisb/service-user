e5900ecad6fad696a06e21279ef3ff48ad5f7b6d
周建威 <361340272@qq.com>
2025-01-05 14:42:54 +0800

    feat(ui): fixed notice message margin

--- src/pages/index/components/MessageItem/components/Notice.tsx
+++ src/pages/index/components/MessageItem/components/Notice.tsx
@@ -5,7 +5,7 @@ import {View} from '@tarojs/components'
 const Notice: React.FC<{
   message: APP.Message
 }> = ({message}) => {
-  return <View className={"w-full text-center text-xs"}>
+  return <View className={"w-full text-center text-xs mt-2"}>
     <View className={"bg-[#E1E1E1] text-[##4A4A4A] inline-block px-1 rounded max-w-[70%]"}>{message.content}</View>
   </View>
 }
