10940986658870f8a92a735da953349e7f993037
zhoujianwei <zhoujianwei@sailvan.com>
2025-01-10 16:04:15 +0800

    u

--- src/pages/index/components/MessageItem/components/Notice.tsx
+++ src/pages/index/components/MessageItem/components/Notice.tsx
@@ -6,7 +6,7 @@ const Notice: React.FC<{
   message: APP.Message
 }> = ({message}) => {
   return <View className={"w-full text-center text-xs mt-2"}>
-    <View className={"bg-[#E1E1E1] text-[##4A4A4A] inline-block px-1 rounded max-w-[70%]"}>{message.content}</View>
+    <View className={"bg-[#E1E1E1] text-[##4A4A4A] inline-block px-1 rounded max-w-[70%] p-0.5"}>{message.content}</View>
   </View>
 }
 export default Notice
